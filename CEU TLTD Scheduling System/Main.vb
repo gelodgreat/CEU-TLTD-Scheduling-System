1cc3cb6aa2f77fa1f2acdb8e80366d83d44afb4f
AngeloDesktop <AngeloDesktop@ANGELODESKTOP>
2016-10-02 16:47:33 +0800

    nnn

--- CEU TLTD Scheduling System/Main.vb
+++ CEU TLTD Scheduling System/Main.vb
@@ -4274,12 +4274,13 @@ End Sub
         End If
     End Sub
 
-    Private Sub main_rgv_recordedacademicsonly_KeyDown(sender As Object, e As KeyEventArgs) Handles main_rgv_recordedacademicsonly.KeyDown,main_rgv_recordedacademicsonly.KeyUp,main_rgv_recordedacademicsonly.KeyDown
-        If Keys.Up Or Keys.Down
-            If main_rgv_recordedacademicsonly.Rows(0).Index = -1
+    Private Sub main_rgv_recordedacademicsonly_KeyDown(sender As Object, e As KeyEventArgs) Handles main_rgv_recordedacademicsonly.KeyDown, main_rgv_recordedacademicsonly.KeyUp, main_rgv_recordedacademicsonly.KeyDown
+        If Keys.Up Or Keys.Down Then
+
+            If main_rgv_recordedacademicsonly.Rows(0).Index = -1 Then
                 'DO NOTHING
             Else
-                main_window_keepSelectedRowIndexAfterUpdate=main_rgv_recordedacademicsonly.SelectedRows(0).Index
+                main_window_keepSelectedRowIndexAfterUpdate = main_rgv_recordedacademicsonly.SelectedRows(0).Index
             End If
         End If
     End Sub
