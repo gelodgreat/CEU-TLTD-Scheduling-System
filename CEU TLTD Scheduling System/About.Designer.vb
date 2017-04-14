ecc10db8019c3ac0e34454bee6f528457d2e2d65
regularuser <spongebobjenkin@yahoo.com>
2017-04-14 13:18:20 +0800

    Version History added as suggested BY @andrewhamili and @gelodgreat

--- CEU TLTD Scheduling System/About.Designer.vb
+++ CEU TLTD Scheduling System/About.Designer.vb
@@ -22,10 +22,13 @@ Partial Class About
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(About))
         Me.RadLabel1 = New Telerik.WinControls.UI.RadLabel()
         Me.RadLabel2 = New Telerik.WinControls.UI.RadLabel()
+        Me.RadTextBox1 = New Telerik.WinControls.UI.RadTextBox()
         CType(Me.RadLabel1,System.ComponentModel.ISupportInitialize).BeginInit
         CType(Me.RadLabel2,System.ComponentModel.ISupportInitialize).BeginInit
+        CType(Me.RadTextBox1,System.ComponentModel.ISupportInitialize).BeginInit
         CType(Me,System.ComponentModel.ISupportInitialize).BeginInit
         Me.SuspendLayout
         '
@@ -34,7 +37,7 @@ Partial Class About
         Me.RadLabel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom),System.Windows.Forms.AnchorStyles)
         Me.RadLabel1.BackColor = System.Drawing.Color.Transparent
         Me.RadLabel1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
-        Me.RadLabel1.Location = New System.Drawing.Point(50, 21)
+        Me.RadLabel1.Location = New System.Drawing.Point(112, 12)
         Me.RadLabel1.Name = "RadLabel1"
         Me.RadLabel1.Size = New System.Drawing.Size(195, 21)
         Me.RadLabel1.TabIndex = 5
@@ -47,7 +50,7 @@ Partial Class About
         Me.RadLabel2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom),System.Windows.Forms.AnchorStyles)
         Me.RadLabel2.BackColor = System.Drawing.Color.Transparent
         Me.RadLabel2.Font = New System.Drawing.Font("Segoe UI", 9.5!)
-        Me.RadLabel2.Location = New System.Drawing.Point(124, 97)
+        Me.RadLabel2.Location = New System.Drawing.Point(327, 13)
         Me.RadLabel2.Name = "RadLabel2"
         Me.RadLabel2.Size = New System.Drawing.Size(46, 20)
         Me.RadLabel2.TabIndex = 6
@@ -55,13 +58,27 @@ Partial Class About
         Me.RadLabel2.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter
         Me.RadLabel2.ThemeName = "VisualStudio2012Dark"
         '
+        'RadTextBox1
+        '
+        Me.RadTextBox1.AutoSize = false
+        Me.RadTextBox1.Location = New System.Drawing.Point(67, 57)
+        Me.RadTextBox1.Multiline = true
+        Me.RadTextBox1.Name = "RadTextBox1"
+        Me.RadTextBox1.ReadOnly = true
+        Me.RadTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.RadTextBox1.Size = New System.Drawing.Size(351, 177)
+        Me.RadTextBox1.TabIndex = 7
+        Me.RadTextBox1.Text = resources.GetString("RadTextBox1.Text")
+        Me.RadTextBox1.ThemeName = "VisualStudio2012Dark"
+        '
         'About
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CausesValidation = false
-        Me.ClientSize = New System.Drawing.Size(296, 227)
+        Me.ClientSize = New System.Drawing.Size(486, 284)
         Me.ControlBox = false
+        Me.Controls.Add(Me.RadTextBox1)
         Me.Controls.Add(Me.RadLabel2)
         Me.Controls.Add(Me.RadLabel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
@@ -79,6 +96,7 @@ Partial Class About
         Me.TopMost = true
         CType(Me.RadLabel1,System.ComponentModel.ISupportInitialize).EndInit
         CType(Me.RadLabel2,System.ComponentModel.ISupportInitialize).EndInit
+        CType(Me.RadTextBox1,System.ComponentModel.ISupportInitialize).EndInit
         CType(Me,System.ComponentModel.ISupportInitialize).EndInit
         Me.ResumeLayout(false)
         Me.PerformLayout
@@ -86,5 +104,6 @@ Partial Class About
 End Sub
     Friend WithEvents RadLabel1 As Telerik.WinControls.UI.RadLabel
     Friend WithEvents RadLabel2 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents RadTextBox1 As Telerik.WinControls.UI.RadTextBox
 End Class
 
